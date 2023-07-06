9b5289f4a05570da066b4c9758fa97f358bb640d
Jibekxoxo <jibek.jorobekova@gmail.com>
2023-07-06 23:46:40 +0600

    req.txt

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.store, name="store"),
@@ -7,5 +8,8 @@ urlpatterns = [
     path('checkout/', views.checkout, name="checkout"),
     path('update_Item/', views.updateItem, name="update_Item"),
     path('process_order/', views.processOrder, name="process_order"),
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('login/register/', views.RegisterPage.as_view(), name="register"),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     
 ]
--- store/views.py
+++ store/views.py
@@ -3,6 +3,12 @@ from .models import *
 from django.http import JsonResponse
 import json
 import datetime
+from .forms import *
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.contrib.auth import logout, login
+
+from django.urls import reverse_lazy
 
 def store(request):
     if request.user.is_authenticated:
@@ -21,7 +27,6 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
@@ -102,6 +107,34 @@ def processOrder(request):
         print('User is not logged in')
     
     return JsonResponse('Payment submitted..', safe=False)
+
+
+class LoginView(LoginView):
+    template_name = 'store/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('store')
+
+
+
+class RegisterPage(FormView):
+    template_name = 'store/register.html'
+    form_class = RegistrationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('store')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super().get(*args, **kwargs)
 
     
 
